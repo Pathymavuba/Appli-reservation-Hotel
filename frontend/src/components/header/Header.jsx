795acd4cc1b9cf009370921fa6f44f5881bd32cc
pathymavuba <mavubapathy@gmail.com>
2022-11-09 17:04:04 +0100

    homepagelist done

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -15,7 +15,7 @@ import "react-date-range/dist/styles.css"; // main css file
 import "react-date-range/dist/theme/default.css"; // theme css file
 import { format } from "date-fns";
 
-const Header = () => {
+const Header = ({type}) => {
   const [openDate, setOpenDate] = useState(false);
   const [date, setDate] = useState([
     {
@@ -38,7 +38,7 @@ const Header = () => {
   }
   return (
     <div className="header">
-      <div className="headerContainer">
+      <div className={type==="list"?"headerContainer listMode":"headerContainer"}>
         <div className="headerList">
           <div className="headerListItem active">
             <FontAwesomeIcon icon={faBed} />
@@ -61,7 +61,7 @@ const Header = () => {
             <span>Airport taxis</span>
           </div>
         </div>
-        <h1 className="headerTitle">A lifetime of discounts? It's Genius </h1>
+       { type !== "list" && <> <h1 className="headerTitle">A lifetime of discounts? It's Genius </h1>
         <p className="headerDesc">
           Get rewarded for your travels – unlock instant savings of 10% or more
           with a free Lamabooking account
@@ -133,12 +133,12 @@ const Header = () => {
               </div>
               </div>
               
-            </div>}
+            </div> }
           </div>
           <div className="headerSearchItem">
             <button className="headerBtn">Search</button>
           </div>
-        </div>
+        </div> </>}
       </div>
     </div>
   );
