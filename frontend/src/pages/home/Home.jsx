3936d65f5d5d431db7eebff3b87e8a4ade1910ff
pathymavuba <mavubapathy@gmail.com>
2022-11-14 15:21:14 +0100

    la page home finie

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Featured from '../../components/featured/Featured'
+import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties'
+import Footer from '../../components/footer/Footer'
 import Header from '../../components/header/Header'
+import MailList from '../../components/mailList/MailList'
 import Navbar from '../../components/navbar/Navbar'
 import PropertyList from '../../components/propertyList/PropertyList'
 import './home.css'
@@ -15,6 +18,9 @@ const Home = () => {
         <h1 className="homeTitle">Browse by property type</h1>
         <PropertyList />
         <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties />
+        <MailList />
+        <Footer />
       </div>
     </div>
   )
